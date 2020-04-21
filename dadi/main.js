691fb40639b1960e444c5bd5610abbaf67adbc24
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-04-21 17:40:22 +0200

    push intermedia

---
+++ dadi/main.js
@@ -0,0 +1,3 @@
+// "Gioco dei dadi" PC vs. Human: chiedi un numero all'utente tra 1 e 6 (giocata human) e estrai un numero casuale tra 1 e 6 (giocata PC). Chi fa il numero più alto vince!
+var acaso = 15223;
+console.log(acaso);
---
+++ mail/main.js
@@ -0,0 +1,19 @@
+// "Sono in lista?" Chiedi all'utente la sua email e controlla che sia nella lista di chi può accedere (ergo: la mail è presente in un array contenente le email "autorizzate") e stampa un messaggio appropriato (sei in lista/non sei in lista)
+
+var email = ["pippo@boolean.it", "plata@boolean.it", "plomo@boolean.it", "topolino@boolean", "paperino@boolean.it", "minnie@boolean.it", "pluto@boolean.it", "pablo@boolean.it"];
+
+email.lenght;
+
+console.log(email.lenght);
+
+
+console.log(email[0]);
+
+
+
+
+for (var i = 0; i < email.lenght; i++){
+
+    console.log(email[i]);
+
+}
