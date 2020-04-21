e72ddc48aaba842f72687b97a9d49c8c249b4022
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-04-21 19:56:21 +0200

    push (dadi completo)

--- dadi/main.js
+++ dadi/main.js
@@ -1,3 +1,16 @@
 // "Gioco dei dadi" PC vs. Human: chiedi un numero all'utente tra 1 e 6 (giocata human) e estrai un numero casuale tra 1 e 6 (giocata PC). Chi fa il numero più alto vince!
-var acaso = 15223;
-console.log(acaso);
+
+var numero_utente = prompt('Inserisci un numero da 1 a 6.')
+console.log('Numero utente:' + numero_utente);
+
+var numero_pc = Math.floor(Math.random() * 6) + 1;
+console.log('Numero pc:' + numero_pc);
+
+
+if (numero_utente > numero_pc) {
+    console.log('Vince utente');
+} else if (numero_utente < numero_pc){
+    console.log('Vince il computer');
+} else {
+    console.log('Il numero è pari');
+}
--- mail/main.js
+++ mail/main.js
@@ -2,18 +2,20 @@
 
 var email = ["pippo@boolean.it", "plata@boolean.it", "plomo@boolean.it", "topolino@boolean", "paperino@boolean.it", "minnie@boolean.it", "pluto@boolean.it", "pablo@boolean.it"];
 
-email.length;
-
-console.log(email.length);
+console.log('Elementi presenti nel array:' + ' ' + email.length);
 
 
-console.log(email[0]);
+var email_utente = prompt('Inserisci la tua email.')
+console.log(email_utente);
 
-
-
-
-for (var i = 0; i < email.length; i++){
-
-    console.log(email[i]);
-
+for (var i = 0; i < email.length; i++) {
+    if (email_utente == email[i]) {
+        console.log('La tua mail è presente nella lista, puoi accedere.');
+    } else if (i == email.length && email_utente != email[7]) {
+        console.log('vkjevkvjekvej');
+    }
 }
+
+
+
+    // console.log('La tua mail non è presente nella lista, non puoi accedere');
